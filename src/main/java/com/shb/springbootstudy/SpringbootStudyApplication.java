b1c9d2ca2d36f40db03d8181b9c515fb92cbde6a
沈涵博 <shenhanbo@ztesa.com.cn>
2021-03-30 16:35:31 +0800

    引入Swagger

--- src/main/java/com/shb/springbootstudy/SpringbootStudyApplication.java
+++ src/main/java/com/shb/springbootstudy/SpringbootStudyApplication.java
@@ -3,9 +3,11 @@ package com.shb.springbootstudy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
-@SpringBootApplication
 @MapperScan(value = "com/shb/springbootstudy/dao")
+@EnableOpenApi
+@SpringBootApplication
 public class SpringbootStudyApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/shb/springbootstudy/controller/HelloController.java
+++ src/main/java/com/shb/springbootstudy/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.shb.springbootstudy.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "hello控制器")
 public class HelloController {
 
     @GetMapping("/hello")
+    @ApiOperation(value = "hello方法")
     public String hello(){
         return "Hello!SpringBoot!";
     }

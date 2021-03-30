0985ab18fcbb217b00016869cac4b6e0894598aa
沈涵博 <shenhanbo@ztesa.com.cn>
2021-03-30 15:26:16 +0800

    引入MyBatisPlus

--- src/main/java/com/shb/springbootstudy/SpringbootStudyApplication.java
+++ src/main/java/com/shb/springbootstudy/SpringbootStudyApplication.java
@@ -1,9 +1,11 @@
 package com.shb.springbootstudy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com/shb/springbootstudy/dao")
 public class SpringbootStudyApplication {
 
     public static void main(String[] args) {

6aa448b18075decedb41e6ef9cde473c4253bd4f
沈涵博 <shenhanbo@ztesa.com.cn>
2021-04-06 14:16:12 +0800

    引入Flyway

---
+++ src/main/resources/db/migration/R__add_unknown_user.sql
@@ -0,0 +1,5 @@
+insert into `user`(user_name,age) values('unknown',77);
+
+insert into `user`(user_name,age) values('unknown',88);
+insert into `user`(user_name,age) values('unknown',99);
+insert into `user`(user_name,age) values('unknown',101010);
